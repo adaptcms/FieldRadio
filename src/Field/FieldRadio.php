6e792894d38927c93e2946ac8ec9a3fca09ebfbf
Charlie Page <charliepage88@gmail.com>
2020-07-05 13:14:09 -0400

    package field validation, misc ui & api fixes, is_required changes

--- src/Field/FieldRadio.php
+++ src/Field/FieldRadio.php
@@ -6,5 +6,15 @@ use Adaptcms\Fields\FieldType;
 
 class FieldRadio extends FieldType
 {
-  //
+  /**
+  * Create Field Rules
+  *
+  * @return array
+  */
+  public function createFieldRules()
+  {
+    return [
+      'meta.options' => 'required'
+    ];
+  }
 }
